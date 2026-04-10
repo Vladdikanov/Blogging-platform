c532dd9b068063277fd9c291bca6a0fde688b52d
Vladislav Dikanov <vladdikanov@gmail.com>
2026-04-10 14:32:46 +0700

    Добавлена возможность ставить и убирать лайк

--- posts/serializers.py
+++ posts/serializers.py
@@ -5,6 +5,8 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.email')
     likes_count = serializers.SerializerMethodField()
+    likes = serializers.SlugRelatedField(many=True, read_only=True, slug_field='email')
+    is_liked = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
@@ -12,3 +14,9 @@ class PostSerializer(serializers.ModelSerializer):
 
     def get_likes_count(self, obj):
         return obj.likes.count()
+    
+    def get_is_liked(self, obj):
+        user = self.context['request'].user
+        if user.is_anonymous:
+            return False
+        return user in obj.likes.all()
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets, permissions
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -10,3 +13,25 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    @action(detail=True, methods=['post'])
+    def like(self, request, pk=None):
+        post = self.get_object()
+        user = request.user
+
+        if user in post.likes.all():
+            return Response({'detail': 'Уже лайкнул'}, status=400)
+
+        post.likes.add(user)
+        return Response({'detail': 'Лайк поставлен'})
+    
+    @action(detail=True, methods=['post'])
+    def unlike(self, request, pk=None):
+        post = self.get_object()
+        user = request.user
+
+        if user not in post.likes.all():
+            return Response({'detail': 'Лайка нет'}, status=400)
+
+        post.likes.remove(user)
+        return Response({'detail': 'Лайк убран'})
